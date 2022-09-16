7b63bdf12cfc7fc6d186919ad7ad42974fd250f8
TharukaAkshan <tharukeakshan19@gmail.com>
2022-09-16 22:43:53 +0530

    Sinhala stemming

--- ML_Model.py
+++ ML_Model.py
@@ -5,16 +5,17 @@ import re
 import nltk
 from nltk.tokenize import word_tokenize
 import io
+nltk.download('punkt')
 
 
-# Load data
+# Load data============================================================================================
 
 Data = pd.read_csv("DataFiles/Sinhala_Singlish_Hate_Speech.csv")
 
 
-# Data Handling Techniques
+# Data Handling Techniques=============================================================================
 
-# Create DataFrame
+# Create DataFrame----------------------
 
 
 df_Data = pd.DataFrame(columns=['PhraseNo', 'Phrase', 'IsHateSpeech'])
@@ -22,7 +23,7 @@ for i in range(Data.shape[0]):
     df_Data.loc[i] = [i + 1] + [Data['Phrase'][i]] + [Data['IsHateSpeech'][i]]
 
 
-# Sinhala Stop Words Removal
+# Sinhala Stop Words Removal----------------------
 
 # getting the stop words
 
@@ -31,7 +32,6 @@ stop_words_Sinhala = []
 for x in sinhala_stop_Words:
     stop_words_Sinhala.append(x.split()[0])
 
-# nltk.download('punkt')
 
 AllDataSinhala = df_Data
 
@@ -45,7 +45,7 @@ for k in range(AllDataSinhala.shape[0]):
     df_Data.loc[k] = [k + 1] + [removing_Sinhala_stopwords] + [AllDataSinhala['IsHateSpeech'][k]]
 
 
-# Romanized Sinhala Stop Words removal
+# Romanized Sinhala Stop Words removal-----------------------------------
 
 # getting the stop words
 singlish_stop_Words = io.open("DataFiles/Singlish StopWords_425.txt", mode="r",
@@ -66,7 +66,7 @@ for r in range(AllDataSinglish.shape[0]):
     df_Data.loc[r] = [r + 1] + [removing_Singlish_stopwords] + [AllDataSinglish['IsHateSpeech'][r]]
 
 
-# Sinhala Steming
+# Sinhala Steming---------------------------------------------------
 
 suffixes = io.open("DataFiles/Suffixes-413.txt", mode="r",encoding="utf-16")
 sinhala_suffixes = []
@@ -86,7 +86,7 @@ for j in range(len(sentences)):
 
 
 
-# Data Cleaning
+# Data Cleaning------------------------------------------------
 
 
 def TagsRemovalText(sentence):
@@ -114,3 +114,59 @@ for i in range(df_Data.shape[0]):
     dataSentence = df_Data['Phrase'][i]
     preprocessData = TagsRemovalText(str(dataSentence))
     df_Data.loc[i] = [i + 1] + [preprocessData] + [df_Data['IsHateSpeech'][i]]
+
+
+# Sinhala Steming------------------------------------------------------
+
+suffixes = io.open("DataFiles/Suffixes-413.txt", mode="r",encoding="utf-16")
+sinhala_suffixes = []
+afterStemingLenghtsUnique = []
+
+for suf in suffixes:
+    sinhala_suffixes.append(suf.strip().split()[0])
+
+
+def isSuffix(s1, s2):
+    n1 = len(s1)
+    n2 = len(s2)
+    if (n1 > n2):
+        return False
+    for i in range(n1):
+        if (s1[n1 - i - 1] != s2[n2 - i - 1]):
+            return False
+    return True
+
+
+def removeSuffix(word, suffix):
+    newLen = len(word) - len(suffix)
+    wordN = word[0:newLen]
+    return wordN
+
+
+def Data_stemming(stemming):
+    stems = {}
+    found = 0
+    for r in range(stemming.shape[0]):
+        Sentence_stem = stemming['Phrase'][r]
+
+        SentenceTokens_stem = word_tokenize(Sentence_stem)
+        stemming_sentence_n = []
+        for wr in SentenceTokens_stem:
+            found = 0
+            for suf in sinhala_suffixes:
+                if (isSuffix(suf.strip(), wr.strip())):
+                    stm = removeSuffix(wr.strip(), suf.strip())
+                    stems[wr] = stm
+                    stemming_sentence_n.append(stems[wr])
+                    found = 1
+                    break
+
+            if (found == 0):
+                stemming_sentence_n.append(wr)
+
+        # load to dataframe
+        df_Data.loc[r] = [r + 1] + [stemming_sentence_n] + [stemming['IsHateSpeech'][r]]
+    return stemming
+
+
+Data_stemming(df_Data)
